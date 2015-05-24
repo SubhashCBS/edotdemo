f8392f60c74c7df966d3b230d7fe3069a057d5c4
PVDPrakash <pprakash63@gmail.com>
2015-05-24 14:26:30 +0530

    gr changes6

--- module/coolroof/src/coolroof/Model/GlassType.php
+++ module/coolroof/src/coolroof/Model/GlassType.php
@@ -18,9 +18,9 @@ namespace coolroof\Model;
          $this->name  = (!empty($data['name'])) ? $data['name'] : null;
 		 $this->code  = (!empty($data['code'])) ? $data['code'] : null;
 		 $this->USI  = (!empty($data['U-SI'])) ? $data['U-SI'] : null;
-		 $this->solar_heat  = (!empty($data['SolarHeat'])) ? $data['Solar Heat'] : null;
+		 $this->solar_heat  = (!empty($data['SolarHeat'])) ? $data['SolarHeat'] : null;
 		 $this->shading  = (!empty($data['Shading'])) ? $data['Shading'] : null;
-		 $this->visible_trans  = (!empty($data['VisibleTrans'])) ? $data['Visible Trans'] : null;
+		 $this->visible_trans  = (!empty($data['VisibleTrans'])) ? $data['VisibleTrans'] : null;
      }
  }
 ?>
